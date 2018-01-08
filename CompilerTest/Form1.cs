e04c11c709fece6dcfb9cfb2146410aeefa0a16e
Илющенко Павел <pavel.ks1996@gmail.com>
2018-01-08 04:25:28 +0300

    Очищен код

--- CompilerTest/Form1.cs
+++ CompilerTest/Form1.cs
@@ -36,13 +36,11 @@ namespace Compiler
             public string Name;
         }
 
-        //public int Indarr;
         public int IndS;
         public int IndR;
 
-        public List<MyWord> Arr = new List<MyWord>();
+        public List<MyWord> SplittedWords = new List<MyWord>();
 
-        //public MyWord[] Arr = new MyWord[1000];
         public MyWord[] ArrS = new MyWord[1000];
 
         public MyRule[] Rule =
@@ -112,35 +110,23 @@ namespace Compiler
             {0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
         };
 
-        public MyUpStr[] ArrNt = new MyUpStr[13];
-
-        public MyUpStr[] ArrStr = new MyUpStr[1000];
-        public string[] ArrM = new string[1000];
-        public int Ind;
         public Form1()
         {
             InitializeComponent();
             textBox1.Text = @"set k 6E-1 for neq div k 9E-3 5E-2 set k mult k 3E-15 set x [ mult k k ] div mult x [ div k k ] or y y mult c sqrt z [ k ] set i 5E-1 for neq i 4E-17 set i div i 5E-8 set y [ div i mult i i ] div mult x [ div i i ] mult 5E-2 sqrt y [ i ] not y [ i ]";
-
-            
-
-
-            
         }
 
         public void Print(int yy, int zz, int xx)
         {
-            int i2;
-            string s1, s2, s3;
-            s1 = "";
-            i2 = yy;
-            while (i2 < Arr.Count)
+            string s1 = "";
+            int i2 = yy;
+            while (i2 < SplittedWords.Count)
             {
-                s1 = s1 + ArrWords[Arr[i2].L].W + " ";
+                s1 = s1 + ArrWords[SplittedWords[i2].L].W + " ";
                 i2 = i2 + 1;
             }
             richTextBox2.Text += @"Строка:" + s1 + '\n';
-            s2 = "";
+            string s2 = "";
             i2 = 0;
             while (i2 <= zz)
             {
@@ -148,7 +134,7 @@ namespace Compiler
                 i2 = i2 + 1;
             }
             richTextBox2.Text += @"Магазин:" + s2 + '\n';
-            s3 = "";
+            string s3 = "";
             i2 = 1;
             while (i2 <= xx)
             {
@@ -197,63 +183,62 @@ namespace Compiler
 
         public void Algorithm()
         {
-            int Tm, Ts, koli, p10, pr11, pr12;
-            Tm = 0;
-            Ts = 0;
+            int tm = 0;
+            int ts = 0;
             int go = 0;
             ArrS[0].L = 18;
             ArrS[0].W = "$";
             IndR = 0;
-            Print(Tm, Ts, IndR);
-            while (Tm <= Arr.Count)
+            Print(tm, ts, IndR);
+            while (tm <= SplittedWords.Count)
             {
-                if (Arr[Tm].L == 18)
+                if (SplittedWords[tm].L == 18)
                 {
-                    if (Ts == 1)
+                    if (ts == 1)
                     {
-                        if ((ArrS[Ts].L == 0) && (ArrS[Ts - 1].L == 18))
+                        if ((ArrS[ts].L == 0) && (ArrS[ts - 1].L == 18))
                         {
                             go = 4;
                         }
                     }
                 }
-                if (((ArrZ[ArrS[Ts].L, Arr[Tm].L] == 1) && go != 4) || ((ArrZ[ArrS[Ts].L, Arr[Tm].L] == 2) && go != 4))
+                if (((ArrZ[ArrS[ts].L, SplittedWords[tm].L] == 1) && go != 4) || ((ArrZ[ArrS[ts].L, SplittedWords[tm].L] == 2) && go != 4))
                 {
-                    Ts = Ts + 1;
-                    ArrS[Ts].L = Arr[Tm].L;
-                    ArrS[Ts].W = Arr[Tm].W;
-                    Tm = Tm + 1;
-                    Print(Tm, Ts, IndR);
+                    ts = ts + 1;
+                    ArrS[ts].L = SplittedWords[tm].L;
+                    ArrS[ts].W = SplittedWords[tm].W;
+                    tm = tm + 1;
+                    Print(tm, ts, IndR);
                     go = 2;
                 }
-                if ((ArrZ[ArrS[Ts].L, Arr[Tm].L] == 3) && go != 2 && go != 4)
+                if ((ArrZ[ArrS[ts].L, SplittedWords[tm].L] == 3) && go != 2 && go != 4)
                 {
-                    koli = 0;
-                    while (ArrZ[ArrS[Ts - koli].L, ArrS[Ts - koli + 1].L] != 1)
+                    int koli = 0;
+                    while (ArrZ[ArrS[ts - koli].L, ArrS[ts - koli + 1].L] != 1)
                     {
                         koli = koli + 1;
                     }
-                    for (p10 = 0; p10 < 16; p10++)
+                    for (var p10 = 0; p10 < 16; p10++)
                     {
                         if (Rule[p10].L == koli && go != 2)
                         {
-                            pr11 = 0;
-                            for (pr12 = 0; pr12 < koli; pr12++)
+                            int pr11 = 0;
+                            for (var pr12 = 0; pr12 < koli; pr12++)
                             {
-                                if (Rule[p10].M[pr12] == ArrS[Ts - koli + 1 + pr12].L)
+                                if (Rule[p10].M[pr12] == ArrS[ts - koli + 1 + pr12].L)
                                 {
                                     pr11 = pr11 + 1;
                                 }
                             }
                             if (pr11 == koli)
                             {
-                                MyCompil(p10, Ts);
-                                Ts = Ts - koli + 1;
-                                ArrS[Ts].L = Rule[p10].P - 1;
-                                ArrS[Ts].W = ArrWords[(ArrS[Ts].L)].W;
+                                MyCompil(p10, ts);
+                                ts = ts - koli + 1;
+                                ArrS[ts].L = Rule[p10].P - 1;
+                                ArrS[ts].W = ArrWords[(ArrS[ts].L)].W;
                                 IndR = IndR + 1;
                                 Rules[IndR] = p10;
-                                Print(Tm, Ts, IndR);
+                                Print(tm, ts, IndR);
                                 go = 2;
                             }
                         }
@@ -264,14 +249,14 @@ namespace Compiler
                     richTextBox2.Text = @"Ошибка при выполнении восходящего разбора!";
                     textBox2.Text = "";
                     go = 3;
-                    Tm = 10000;
+                    tm = 10000;
                     for (int i = 0; i < ArrS.Length; i++)
                     {
                         ArrS[i].L = 0;
                         ArrS[i].T = "";
                         ArrS[i].W = "";
                     }
-                    Arr.Clear();
+                    SplittedWords.Clear();
                     for (int i = 0; i < Rules.Length; i++)
                     {
                         Rules[i] = 0;
@@ -284,14 +269,14 @@ namespace Compiler
                 }
                 if (go == 4)
                 {
-                    Tm = 10000;
+                    tm = 10000;
                     for (int i = 0; i < ArrS.Length; i++)
                     {
                         ArrS[i].L = 0;
                         ArrS[i].T = "";
                         ArrS[i].W = "";
                     }
-                    Arr.Clear();
+                    SplittedWords.Clear();
                     for (int i = 0; i < Rules.Length; i++)
                     {
                         Rules[i] = 0;
@@ -303,11 +288,10 @@ namespace Compiler
         }
         public int IsNumber(string str2, string dopstr, int k)
         {
-            int iii, pr2;
-            pr2 = 0;
-            for (iii = 0; iii < dopstr.Length; iii++)
+            int pr2 = 0;
+            for (var i = 0; i < dopstr.Length; i++)
             {
-                if (str2[k + 1] == dopstr[iii]) pr2 = pr2 + 1;
+                if (str2[k + 1] == i) pr2 = pr2 + 1;
             }
             if (pr2 == 0) return 0;
             else return 1;
@@ -315,23 +299,22 @@ namespace Compiler
 
         public int IsThisNumber(string str1, int nach1, int kon1)
         {
-            int jjj, pr3;
             if (str1.Substring(nach1, kon1 + 1 - nach1) == " true" || str1.Substring(nach1, kon1 + 1 - nach1) == " false")
             {
-                Arr.Add(new MyWord(17, str1.Substring(nach1, kon1 + 1 - nach1)));
+                SplittedWords.Add(new MyWord(17, str1.Substring(nach1, kon1 + 1 - nach1)));
                 return 1;
             }
             else
             if (IsNumber(str1, "0123456789", nach1) == 1)
             {
-                pr3 = 0;
-                for (jjj = nach1; jjj < kon1; jjj++)
+                int pr3 = 0;
+                for (var jjj = nach1; jjj < kon1; jjj++)
                 {
                     if (IsNumber(str1, "0123456789.Ee-", jjj) == 1) pr3 = pr3 + 1;
                 }
                 if (pr3 == kon1 - nach1)
                 {
-                    Arr.Add(new MyWord(17, str1.Substring(nach1, kon1 + 1 - nach1)));
+                    SplittedWords.Add(new MyWord(17, str1.Substring(nach1, kon1 + 1 - nach1)));
 
                     return 1;
                 }
@@ -347,23 +330,22 @@ namespace Compiler
                 return 0;
             }
         }
-        public int IsThisOperator(string str1, int nach1, int kon1)
+        public int IsThisOperator(string str, int startPos, int endPos)
         {
-            int ii, jj, kol;
-            if (str1[nach1] == ' ') nach1 += 1;
-            for (ii = 6; ii < 16; ii++)
+            if (str[startPos] == ' ') startPos += 1;
+            for (var i = 6; i < 16; i++)
             {
-                if (kon1 - nach1 + 1 == (ArrWords[ii].W).Length)
+                if (endPos - startPos + 1 == (ArrWords[i].W).Length)
                 {
-                    kol = 0;
-                    for (jj = 1; jj <= (ArrWords[ii].W).Length; jj++)
+                    int kol = 0;
+                    for (var j = 1; j <= (ArrWords[i].W).Length; j++)
                     {
-                        if (str1[nach1 + jj - 1] == ArrWords[ii].W[jj - 1]) kol = kol + 1;
+                        if (str[startPos + j - 1] == ArrWords[i].W[j - 1]) kol = kol + 1;
                     }
-                    if (kon1 - nach1 + 1 == kol)
+                    if (endPos - startPos + 1 == kol)
                     {
-                        Arr.Add(new MyWord(ii, ArrWords[ii].W));
-                        return ii;
+                        SplittedWords.Add(new MyWord(i, ArrWords[i].W));
+                        return i;
                     }
                 }
             }
@@ -372,16 +354,16 @@ namespace Compiler
 
         public void Up()
         {
-            int i, nach, probel, dop1, k = 0;
+            int k = 0;
             ArrS[1].T = "";
             richTextBox2.Clear();
             textBox2.Clear();
             Str += textBox1.Text;
             Str += " ";
             IndR = 0;
-            nach = 0;
-            probel = 1;
-            for (i = 0; i < Str.Length; i++)
+            int nach = 0;
+            int probel = 1;
+            for (var i = 0; i < Str.Length; i++)
             {
                 if (Str[i] == ' ')
                     if (probel == 0)
@@ -389,12 +371,12 @@ namespace Compiler
                         probel = 1;
                         if (IsThisOperator(Str, nach, i - 1) == 0)
                         {
-                            dop1 = IsThisNumber(Str, nach, i - 1);
+                            int dop1 = IsThisNumber(Str, nach, i - 1);
                             if (dop1 == 0)
                             {
                                 if ((Str.Substring(nach + 1, i - nach - 1)).Length <= 8 && (Str.Substring(nach + 1, i - nach - 1)).Length > 0)
                                 {
-                                    Arr.Add(new MyWord(16, Str.Substring(nach, i + 1 - nach)));
+                                    SplittedWords.Add(new MyWord(16, Str.Substring(nach, i + 1 - nach)));
                                 }
                                 else
                                 {
@@ -424,23 +406,13 @@ namespace Compiler
             }
             if (k != 1)
             {
-                Arr.Add(new MyWord(18, "$"));
+                SplittedWords.Add(new MyWord(18, "$"));
                 Algorithm();
             }
         }
         
         private void button1_Click(object sender, EventArgs e)
         {
-            ArrStr = new MyUpStr[1000];
-            for (int i = 0; i < ArrStr.Length; i++)
-            {
-                ArrStr[i].I = 0;
-                ArrStr[i].Name = "";
-            }
-            for (int i = 0; i < ArrM.Length; i++)
-            {
-                ArrM[i] = "";
-            }
             Str = "";
             
             for (int i = 0; i < ArrS.Length; i++)
@@ -449,7 +421,7 @@ namespace Compiler
                 ArrS[i].T = "";
                 ArrS[i].W = "";
             }
-            Arr.Clear();
+            SplittedWords.Clear();
             for (int i = 0; i < Rules.Length; i++)
             {
                 Rules[i] = 0;
